23442f8c0f6862e2dde7dbf01820acca90a8adfc
an-na-kuzmina <anna.bychkova2008@gmail.com>
2024-09-01 02:41:52 +0300

    profile styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,26 +2,26 @@ import s from './Profile.module.css';
 
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div>
-      <div>
-        <img src={image} alt={`${name}'s avatar`} />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={s.cardProfile}>
+      <div className={s.userInfo}>
+        <img src={image} alt={`${name}'s avatar`} className={s.userImg} />
+        <p className={s.userName}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
 
-      <ul>
+      <ul className={s.stats}>
         <li>
           <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span className={s.statValues}>{stats.followers}</span>
         </li>
         <li>
           <span>Views</span>
-          <span>{stats.views}</span>
+          <span className={s.statValues}>{stats.views}</span>
         </li>
         <li>
           <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span className={s.statValues}>{stats.likes}</span>
         </li>
       </ul>
     </div>
